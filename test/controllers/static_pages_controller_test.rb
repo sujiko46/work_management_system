24228f6e2efbbc77c70c34228acef751c1e90dec
sujiko46 <kyosuke.mizukami@jmty.jp>
2020-06-01 15:20:54 +0900

    勤怠管理システムに置き換え

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 end
